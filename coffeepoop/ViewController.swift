6c1656f04b3bcdc9d816881bee7ebd9181599e52
KarissaKae <kgoff@watkins.edu>
2016-11-18 13:20:00 -0600

    Initial Commit

---
+++ coffeepoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coffeepoop
+//
+//  Created by Karissa on 11/18/16.
+//  Copyright © 2016 Karissa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
